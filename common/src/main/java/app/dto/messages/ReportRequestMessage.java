38a70ea668d8e103264d6ae59bbd5d317a3fee9a
Polina <sokpol343@gmail.com>
2026-04-30 21:09:15 +0300

    архитектура для лаб3

--- common/src/main/java/app/dto/messages/ReportRequestMessage.java
+++ common/src/main/java/app/dto/messages/ReportRequestMessage.java
@@ -1,4 +1,4 @@
-package app.dto;
+package app.dto.messages;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReportRequestMessageRequest {
+public class ReportRequestMessage {
     private Long applicationId;
 }
